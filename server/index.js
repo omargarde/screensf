7ab8866c6cfe5e63c777e6bdc3f9fe69112dc7b7
Omar Rodriguez <omar@Omars-MacBook-Air.local>
2018-10-14 16:32:59 -0700

    added port variable and made format changes to server. installed nodemon

--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const db = require('../database/database.js');
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-app.get('/recommended/:id', function(req, res) {
+app.get('/recommended/:id', (req, res) => {
   const items = db.fetchRecommended(req.params.id);
   res.send(JSON.stringify(items));
   res.end();
 });
 
-app.get('/showtimes/:id', function (req, res) {
+app.get('/showtimes/:id', (req, res) => {
   const items = db.fetchShowtimes(req.params.id);
   res.send(JSON.stringify(items));
   res.end();
 });
 
-app.listen(3000, console.log('app is running on 3000'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
