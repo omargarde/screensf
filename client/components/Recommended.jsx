b338d5b9ec22afa82babf25f392551b9f1b8b605
Omar Rodriguez <omar@Omars-MacBook-Air.local>
2018-11-26 15:04:42 -0800

    added featured showtime heading, venue heading in featured details

--- client/components/Recommended.jsx
+++ client/components/Recommended.jsx
@@ -9,14 +9,16 @@ const Recommended = props => (
       className="featured-image"
       alt="featured"
     />
+    <h3>Featured Showtime</h3>
     <div className="featured-details">
       <div className="featured-showtime">
+        <h4>{props.featured.venue}</h4>
         <ShowsList show={props.featured} />
       </div>
       <div className="featured-article">
         {props.featured.article.short}
         <div className="writer">
-         - {props.featured.writer}
+         {props.featured.writer}
         </div>
       </div>
     </div>
