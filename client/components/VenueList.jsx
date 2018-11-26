cfdc4a13077c049cd4bd6758269b8eb0afa4c1d7
Omar Rodriguez <omar@Omars-MacBook-Air.local>
2018-11-26 13:48:16 -0800

    Changed venue headings from h2 to h3. Margin changes around those same h2s and h3s as well.

--- client/components/VenueList.jsx
+++ client/components/VenueList.jsx
@@ -3,7 +3,7 @@ import ShowsList from './ShowsList.jsx';
 
 const VenueList = (props) => (
   <div className="screenings-venues">
-    <h2>{props.venue.venue}</h2>
+    <h3>{props.venue.venue}</h3>
     <div className = "venue-shows">
       {props.venue.shows.map(item => (
         <ShowsList show={item} key={item.film} />
