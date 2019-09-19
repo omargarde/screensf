7923dbdd8955b74d1b6452c93e6d103491cfff75
Omar Rodriguez <omar@Omars-MacBook-Air.local>
2019-09-19 14:58:16 -0700

    database/database.js - added post operations to each table in database
    database/database.sql
    - renamed series_venues to venues_series
    - renamed series_screenings to screenings_series
    - added a few more showtimes and venues examples to insert when initializing the database
    
    server/index.js
    - added post operations to server

--- database/database.js
+++ database/database.js
@@ -11,7 +11,7 @@ const normalizeShowtimes = (showtimes) => {
   return normalizedTimes;
 };
 
-const findRecommendedOnDate = (req, res) => {
+const getRecommendedOnDate = (req, res) => {
   db.query(`SELECT 
     featured_films.featured_image AS image,
     featured_films.author AS writer,
@@ -33,8 +33,8 @@ const findRecommendedOnDate = (req, res) => {
     INNER JOIN screenings ON featured_films.screenings_id=screenings.id
     INNER JOIN movies ON screenings.movies_id=movies.id
     INNER JOIN venues ON screenings.venues_id=venues.id
-    INNER JOIN series_screenings ON screenings.id = series_screenings.screenings_id
-    INNER JOIN series ON series.id = series_screenings.series_id 
+    INNER JOIN screenings_series ON screenings.id = screenings_series.screenings_id
+    INNER JOIN series ON series.id = screenings_series.series_id 
     INNER JOIN showtimes ON showtimes.screenings_id = screenings.id
     WHERE 
     featured_films.ondate = $/today/
@@ -63,7 +63,7 @@ const findRecommendedOnDate = (req, res) => {
 };
 
 
-const findShowtimesOnDate = (req, res) => {
+const getShowtimesOnDate = (req, res) => {
   db.query(`SELECT 
     venues.title AS venue,
     venues.short_title AS venueShortTitle,
@@ -81,8 +81,8 @@ const findShowtimesOnDate = (req, res) => {
     screenings
     INNER JOIN movies ON screenings.movies_id=movies.id
     INNER JOIN venues ON screenings.venues_id=venues.id
-    INNER JOIN series_screenings ON screenings.id = series_screenings.screenings_id
-    INNER JOIN series ON series.id = series_screenings.series_id 
+    INNER JOIN screenings_series ON screenings.id = screenings_series.screenings_id
+    INNER JOIN series ON series.id = screenings_series.series_id 
     INNER JOIN showtimes ON showtimes.screenings_id = screenings.id
     WHERE
     screenings.start_date <= $/today/ AND screenings.end_date >= $/today/
@@ -122,5 +122,204 @@ const findShowtimesOnDate = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  db.query(`
+    INSERT INTO 
+    movies 
+    (id, title, director, year, duration) 
+    VALUES 
+    ($/id/, $/title/, $/director/, $/year/, $/duration/);`,
+  {
+    id: req.body.id,
+    title: req.body.title,
+    director: req.body.director,
+    year: req.body.year,
+    duration: req.body.duration,
+  })
+    .then(() => {
+      console.log('successful post');
+      res.end();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
 
-module.exports = { findShowtimesOnDate, findRecommendedOnDate };
+  res.end();
+};
+
+const postSeries = (req, res) => {
+  db.query(`
+    INSERT INTO 
+    series 
+    (id, title) 
+    VALUES 
+    (DEFAULT, $/title/);`,
+  {
+    title: req.body.title,
+  })
+    .then(() => {
+      console.log('successful post');
+      res.end();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  res.end();
+};
+
+const postVenue = (req, res) => {
+  db.query(`
+    INSERT INTO 
+    venues 
+    (id, title, short_title, city) 
+    VALUES 
+    (DEFAULT, $/title/, $/short_title/, $/city/);`,
+  {
+    title: req.body.title,
+    short_title: req.body.short_title,
+    city: req.body.city,
+  })
+    .then(() => {
+      console.log('successful post');
+      res.end();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  res.end();
+};
+
+const postScreening = (req, res) => {
+  db.query(`
+    INSERT INTO 
+    screenings 
+    (id, movies_id, venues_id, screening_url, start_date, end_date, format, screening_note) 
+    VALUES 
+    (DEFAULT, $/movies_id/, $/venues_id/, $/screening_url/, $/start_date/, $/end_date/, $/format/, $/screening_note/);`,
+  {
+    movies_id: req.body.movies_id,
+    venues_id: req.body.venues_id,
+    screening_url: req.body.screening_url,
+    start_date: req.body.start_date,
+    end_date: req.body.end_date,
+    format: req.body.format,
+    screening_note: req.body.screening_note,
+  })
+    .then(() => {
+      console.log('successful post');
+      res.end();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  res.end();
+};
+
+const postShowtimes = (req, res) => {
+  db.query(`
+    INSERT INTO 
+    showtimes 
+    (id, screenings_id, showtime, showtime_note)
+    VALUES 
+    (DEFAULT, $/screenings_id/, $/showtime/, $/showtime_note/);`,
+  {
+    screenings_id: req.body.screenings_id,
+    showtime: req.body.showtime,
+    showtime_note: req.body.showtime_note,
+  })
+    .then(() => {
+      console.log('successful post');
+      res.end();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  res.end();
+};
+
+const postScreeningsSeries = (req, res) => {
+  db.query(`
+    INSERT INTO 
+    screenings_series
+    (id, screenings_id, series_id)
+    VALUES 
+    (DEFAULT, $/screenings_id/, $/series_id/);`,
+  {
+    screenings_id: req.body.screenings_id,
+    series_id: req.body.series_id,
+  })
+    .then(() => {
+      console.log('successful post');
+      res.end();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  res.end();
+};
+
+const postVenuesSeries = (req, res) => {
+  db.query(`
+    INSERT INTO 
+    venues_series 
+    (id, venues_id, series_id)
+    VALUES 
+    (DEFAULT, $/venues_id/, $/series_id/);`,
+  {
+    venues_id: req.body.venues_id,
+    series_id: req.body.series_id,
+  })
+    .then(() => {
+      console.log('successful post');
+      res.end();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  res.end();
+};
+
+
+const postFeaturedScreening = (req, res) => {
+  db.query(`
+    INSERT INTO 
+    featured_films
+    (id, screenings_id, ondate, featured_image, author, article)
+    VALUES 
+    (DEFAULT, $/screenings_id/, $/ondate/, $/featured_image/, $/author/, $/article/);`,
+  {
+    screenings_id: req.body.screenings_id,
+    ondate: req.body.ondate,
+    featured_image: req.body.featured_image,
+    author: req.body.author,
+    article: req.body.article,
+  })
+    .then(() => {
+      console.log('successful post');
+      res.end();
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  res.end();
+};
+
+module.exports = {
+  getShowtimesOnDate,
+  getRecommendedOnDate,
+  postMovie,
+  postSeries,
+  postVenue,
+  postScreening,
+  postShowtimes,
+  postScreeningsSeries,
+  postVenuesSeries,
+  postFeaturedScreening,
+};
