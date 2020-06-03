1a0cb9d655d369ad0f1f43bb90700f82712b54bd
Omar Rodriguez <omar@Omars-MacBook-Air.local>
2020-06-02 22:16:01 -0700

    babel configs fixed

--- babel.config.js
+++ babel.config.js
@@ -6,19 +6,3 @@ module.exports = {
       '@babel/plugin-syntax-dynamic-import'
     ]
   };
-  1
-  2
-  3
-  4
-  5
-  6
-  7
-  8
-  module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: [
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-object-rest-spread',
-      '@babel/plugin-syntax-dynamic-import'
-    ]
-  };
