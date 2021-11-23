425fc8d227469ed590f80f0a08beaa3c1df49b92
Omar Rodriguez <tromar@gmail.com>
2021-11-23 15:26:57 -0800

    changing meta tags in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,12 @@ import Venues from './components/venues/Venues';
 import About from './components/About';
 import VenueView from './components/venues/VenueView';
 import ScrollToTop from './components/ScrollToTop';
-import { Helmet } from 'react-helmet';
-
 // eslint-disable-next-line
 import styles from './styles/main';
 
 function App() {
   return (
     <div>
-      <Helmet>
-          <meta charset="UTF-8"/>
-          <title>SF Bay Film</title>
-          <meta property="og:title" content="SF Bay Film" data-react-helmet="true"/>
-          <meta property="og:url" content="http://sfbayfilm.com/" data-react-helmet="true"/>
-          <meta property="og:image" content="https://storage.googleapis.com/filmcans/true-romance-banner.jpg" data-react-helmet="true"/>
-          <meta property="og:description" content="SF Bay Film is a listing of daily showtimes for independent theaters, repertory cinema, and select film series and festivals in the San Francisco Bay Area." data-react-helmet="true"/>
-          <meta property="og:type" content="website" data-react-helmet="true"/>
-      </Helmet>
       <Nav />
       <div className="wrapper">
         <Switch>
